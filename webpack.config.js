127277775fb4af3b68461131d38f3a6ff04ee468
shaoyuee <chunjian.sye@raycloud.com>
2019-05-15 16:21:58 +0800

    webpack学习

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle-[hash].js"//打包后输出文件的文件名
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        port: 9000,
+        historyApiFallback: true,//不跳转
+        inline: true, //实时刷新
+        hot: true
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            verbose: true,
+            dry: false
+        }),
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/index.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin() //热加载插件
+    ],
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true, // 指定启用css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                        }
+                    },
+                    {
+                        loader: "postcss-loader"
+                    }
+                ]
+            },
+        ]
+    }
+};
